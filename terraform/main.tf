872cc58860091003b2055b53f04bef18c3358bec
Manju <manjukrishna.m@codework.ai>
2026-02-04 12:45:53 +0530

    git rebase --continue Finalize Terraform config and enable GitHub Actions deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,19 @@
+################################
+# Random ID (to avoid SG duplicate name error)
+################################
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+################################
+# Security Group for FastAPI
+################################
 resource "aws_security_group" "fastapi_sg" {
-  name = "fastapi-sg"
+  name        = "fastapi-sg-${random_id.suffix.hex}"
+  description = "Security group for FastAPI application"
 
   ingress {
+    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -9,6 +21,7 @@ resource "aws_security_group" "fastapi_sg" {
   }
 
   ingress {
+    description = "Allow FastAPI"
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
@@ -16,35 +29,31 @@ resource "aws_security_group" "fastapi_sg" {
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_instance" "fastapi_ec2" {
-  ami           = "ami-0f5ee92e2d63afc18"   # Amazon Linux 2 (Mumbai)
-  instance_type = "t2.micro"
-  key_name = "my-key"
-
-  vpc_security_group_ids = [aws_security_group.fastapi_sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install python3 git -y
-
-              cd /home/ec2-user
-              git clone https://github.com/manju-209/fastapi-terraform-ec2.git
-              cd fastapi-terraform-ec2
-
-              pip3 install -r requirements.txt
-
-              nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
-              EOF
 
   tags = {
-    Name = "FastAPI-EC2"
+    Name = "fastapi-sg"
+  }
+}
+
+################################
+# EC2 Instance
+################################
+resource "aws_instance" "fastapi_ec2" {
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (ap-south-1)
+  instance_type = "t2.micro"
+  key_name      = "fastapi-key"
+
+  vpc_security_group_ids = [
+    aws_security_group.fastapi_sg.id
+  ]
+
+  tags = {
+    Name = "fastapi-ec2"
   }
 }
